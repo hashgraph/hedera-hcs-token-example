b53e3fc8184a6a06ea1abeb9c232f1d25cb68140
gregscullard <greg@scullard.com>
2020-06-03 18:21:32 +0200

    npm updates (#21)
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- javascript/hedera-hcs-token-example-ui/build/webpack.base.conf.js
+++ javascript/hedera-hcs-token-example-ui/build/webpack.base.conf.js
@@ -37,19 +37,19 @@ module.exports = {
           {
             loader: 'sass-loader',
             // Requires sass-loader@^7.0.0
-            // options: {
-            //   implementation: require('sass'),
-            //   fiber: require('fibers'),
-            //   indentedSyntax: true // optional
-            // },
-            // Requires sass-loader@^8.0.0
             options: {
               implementation: require('sass'),
-              sassOptions: {
-                fiber: require('fibers'),
-                indentedSyntax: true // optional
-              },
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
             },
+            // Requires sass-loader@^8.0.0
+            // options: {
+            //   implementation: require('sass'),
+            //   sassOptions: {
+            //     fiber: require('fibers'),
+            //     indentedSyntax: true // optional
+            //   },
+            // },
           },
         ],
       },
